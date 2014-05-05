9340bcdfc7a967e791a221a58b04ffdbf446bf29
Waldemar Toews <toews@erp-d300.opendynamic.local>
2014-05-05 15:34:39 +0200

    SEPA-Überweisungen: Sortieren nach Fälligkeitsdatum.
    Schritt 1 bei der Überweisung via SEPA:
    Liste nach dem Fälligkeitsdatum sortieren.

--- SL/SEPA.pm
+++ SL/SEPA.pm
@@ -69,7 +69,7 @@ sub retrieve_open_invoices {
        WHERE (${arap}.amount > (COALESCE(open_transfers.amount, 0) + ${arap}.paid))
          AND (${arap}.direct_debit <> ${tf})
 
-       ORDER BY lower(vc.name) ASC, lower(${arap}.invnumber) ASC
+       ORDER BY ${arap}.duedate ASC, lower(vc.name) ASC, lower(${arap}.invnumber) ASC
 |;
 
   my $results = selectall_hashref_query($form, $dbh, $query);
